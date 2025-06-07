68e1cab9c3344a5e2ac9a9ea6d2f157e4e510e54
Ryan Gibson <ryan.l.gibson@protonmail.com>
2025-06-06 22:25:07 -0400

    asyncAwaitVersion added with updated bash script.

---
+++ asyncAwaitVersion.js
@@ -0,0 +1,78 @@
+const { getName } = require("./getName");
+const userName = getName();
+
+doSummerChores(userName);
+
+function fellAsleep() {
+  return Math.random() < 0.2;
+}
+
+function mowYard(name) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(name + " mowed the yard");
+      resolve();
+    }, 2000);
+  });
+}
+
+function weedEat(name) {
+  return new Promise((resolve, reject) => {
+    if (fellAsleep()) {
+      return reject(name + " got tired and fell asleep");
+    }
+    setTimeout(() => {
+      console.log(name + " finished using the weed eater.");
+      resolve();
+    }, 1500);
+  });
+}
+
+function trimHedges(name) {
+  return new Promise((resolve, reject) => {
+    if (fellAsleep()) {
+      return reject(name + " got tired and fell asleep");
+    }
+    setTimeout(() => {
+      console.log(name + " finished trimming the hedges.");
+      resolve();
+    }, 1000);
+  });
+}
+
+function collectWood(name) {
+  return new Promise((resolve, reject) => {
+    if (fellAsleep()) {
+      return reject(name + " got tired and fell asleep");
+    }
+    setTimeout(() => {
+      console.log(name + " finished collecting the wood.");
+      resolve();
+    }, 2500);
+  });
+}
+
+function waterGarden(name) {
+  return new Promise((resolve, reject) => {
+    if (fellAsleep()) {
+      return reject(name + " got tired and fell asleep");
+    }
+    setTimeout(() => {
+      console.log(name + " finished watering the garden.");
+      resolve();
+    }, 500);
+  });
+}
+
+async function doSummerChores(name) {
+  try {
+    await mowYard(name);
+    await weedEat(name);
+    await trimHedges(name);
+    await collectWood(name);
+    await waterGarden(name);
+    console.log(name + " finished all their chores!")
+  } catch(error) {
+    console.log(error);
+  }
+}
--- runChores.sh
+++ runChores.sh
@@ -4,29 +4,33 @@ while true; do
   echo "Choose an option:"
   echo "1. Run callbackVersion.js"
   echo "2. Run promiseVersion.js"
-  echo -e "3. Exit\n"
+  echo "3. Run asyncAwaitVersion.js"
+  echo -e "4. Exit\n"
 
-  read -p "Enter your choice (1, 2, or 3): " choice
+  read -p "Enter your choice (1, 2, 3, 4): " choice
 
   case $choice in
-    1|2)
-      read -p "Enter your name: " name
-      if [ "$choice" -eq 1 ]; then
-        echo "Running callbackVersion.js..."
-        node callbackVersion.js "$name"
-      else
-        echo "Running promiseVersion.js..."
-        node promiseVersion.js "$name"
-      fi
-      break
-      ;;
-    3)
-      echo "Goodbye!"
-      exit 0
-      ;;
-    *)
-      echo -e "\nInvalid choice. Please select 1, 2, or 3.\n"
-      sleep 1
-      ;;
+  1 | 2 | 3)
+    read -p "Enter your name: " name
+    if [ "$choice" -eq 1 ]; then
+      echo "Running callbackVersion.js..."
+      node callbackVersion.js "$name"
+    elif [ "$choice" -eq 2 ]; then
+      echo "Running promiseVersion.js..."
+      node promiseVersion.js "$name"
+    else
+      echo "Running asyncAwaitVersion.js..."
+      node asyncAwaitVersion.js "$name"
+    fi
+    break
+    ;;
+  4)
+    echo "Goodbye!"
+    exit 0
+    ;;
+  *)
+    echo -e "\nInvalid choice. Please select 1, 2, or 3.\n"
+    sleep 1
+    ;;
   esac
 done
